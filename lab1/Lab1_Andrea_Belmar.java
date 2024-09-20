5ee89181f4dd5ead089bebaba8e1994ee3e7fbf7
andy33366 <andy704389@gmail.com>
2024-09-19 21:14:48 -0500

    added the lab 1 files

---
+++ lab1/Lab1_Andrea_Belmar.java
@@ -0,0 +1,19 @@
+/*
+ * Andrea Belmar
+ *
+ */
+
+
+public class Lab1_Andrea_Belmar {
+
+	static public void main(String[] args) {
+
+		String message;
+
+		message="I would like to be a programmer because I will be able to make cool programs";
+
+		System.out.println(message);
+
+	
+	}
+}
