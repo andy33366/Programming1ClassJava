c3a12f022d34e7fcde65b64ca0cedef41159a764
andy33366 <andy704389@gmail.com>
2024-09-26 19:02:03 -0500

    removed the inclass file and added the Lab5 file

---
+++ lab5/Lab5_Andrea_Belmar.java
@@ -0,0 +1,22 @@
+/*
+ *
+ *
+ *
+ */
+
+import java.util.Scanner;
+
+public class Lab5_Andrea_Belmar{
+
+	static public void main(String[] args){
+	
+		//initialializes the scanner
+		Scanner input = new Scanner(System.in);
+		
+		int userInput, div16Result, mod16Result;
+		String
+
+
+
+	}
+}
