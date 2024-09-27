e752196e82f315f264203c74d2ae8c2a1a3c9977
andy33366 <andy704389@gmail.com>
2024-09-26 19:36:13 -0500

    lab5 attempt 1

--- lab5/Lab5_Andrea_Belmar.java
+++ lab5/Lab5_Andrea_Belmar.java
@@ -14,7 +14,76 @@ public class Lab5_Andrea_Belmar{
 		Scanner input = new Scanner(System.in);
 		
 		int userInput, div16Result, mod16Result;
-		String
+		String mostSigDigit, leastSigDigit;
+
+		//prompts user for the amount of red
+		System.out.println("Enter the amount of red as an integer from 0 - 255: ");
+		userInput = input.nextInt();
+
+		//checks in integer is between 0-255
+		if (userInput <= 255 && userInput >= 0){
+		
+			//valid number
+			
+			//divides the userInput by 16 and assigns a value for the most significant figure
+			div16Result = userInput / 16;
+			switch (div16Result){
+			
+				case 10:
+					mostSigDigit = "A";
+					break;
+				case 11:
+					mostSigDigit = "B";
+					break;
+				case 12:
+					mostSigDigit = "C";
+					break;
+				case 13:
+					mostSigDigit = "D";
+					break;
+				case 14:
+					mostSigDigit = "E";
+					break;
+				case 15:
+					mostSigDigit = "F";						
+					break;
+				default:
+					mostSigDigit = String.valueOf(div16Result);
+			}
+			
+			//does userInput % 16 to determine the second sig digit
+			mod16Result = userInput % 16;
+			switch (mod16Result){
+				
+				case 10:
+					leastSigDigit = "A";
+					break;
+				case 11:
+					leastSigDigit = "B";
+					break;
+				case 12:
+					leastSigDigit = "C";
+					break;
+				case 13:
+					leastSigDigit = "D";
+					break;
+				case 14:
+					leastSigDigit = "E";
+					break;
+				case 15:
+					leastSigDigit = "F";						
+					break;
+				default:
+					leastSigDigit = String.valueOf(mod16Result);
+			
+			}
+			System.out.println("The hex value is: "+mostSigDig+leastSigDig);
+		}
+		else{
+			//invalid number
+			System.out.println("invalid number");
+		}
+
 
 
 
