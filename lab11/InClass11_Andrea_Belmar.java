02b2ddeb5b8087b3006b96753e6a27af66da3156
andy33366 <andy704389@gmail.com>
2024-11-07 19:00:46 -0600

    added in class 11

---
+++ lab11/InClass11_Andrea_Belmar.java
@@ -0,0 +1,27 @@
+/*
+ *
+ */
+import java.util.Scanner;
+
+public class InClass11_Andrea_Belmar{
+
+	public static void main{
+
+		//prints 250 + 500 and "hello" + "world"
+		System.out.println("Adding 250 and 500 gives: " + add(250, 500));
+		System.out.println("Combining \"Hello\" and \"world\" gives: "add("Hello", " world"));
+	
+	}
+
+	//takes 2 doubles and adds them together
+	public static double add(double a, double b){
+	
+		return a + b;
+	}
+
+	//takes 2 strings and adds them together
+	public static String add(String a, String b){
+	
+		return a + b;
+	}
+}
