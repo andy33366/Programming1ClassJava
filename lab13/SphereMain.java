abe45e7936fb09af728ddd2cb29d1d943cf2a984
andy33366 <andy704389@gmail.com>
2024-12-03 13:53:30 -0600

    finished lab 13

---
+++ lab13/SphereMain.java
@@ -0,0 +1,30 @@
+/*
+ *
+ *In class 13
+ *
+ */
+
+import java.util.Scanner;
+
+public class SphereMain {
+
+	public static void main(String[] args){
+	
+		Scanner input = new Scanner(System.in);
+
+		//constructs a sphere object of raduis = 5
+		Sphere sphere = new Sphere(5);
+
+		System.out.println("The radius is: "+sphere.getRadius());	
+		System.out.println("the surface area is: "+sphere.getSurfaceArea());	
+		System.out.println("The volume is: "+sphere.getVolume());
+		sphere.setRadius(7.5);	
+		System.out.println("The new radius is: "+sphere.getRadius());	
+		System.out.println("the new surface area is: "+sphere.getSurfaceArea());	
+		System.out.println("The new volume is: "+sphere.getVolume());
+
+
+	}
+}
+
+
